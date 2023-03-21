8b07ca73b3173766bbf6222dd39fc801e2bfad8b
Caesar <112681289+toheedakhtar@users.noreply.github.com>
2023-03-21 21:46:13 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -3,8 +3,15 @@ var staticCacheName = "TA";
 self.addEventListener("install", function (e) {
 e.waitUntil(
 	caches.open(staticCacheName).then(function (cache) {
-	return cache.addAll(["/"]);
-	})
+	return cache.addAll([
+		"/",
+		"/assets",
+		"/css",
+		"/index.html",
+		"/serviceworker.js",
+		"/manifest.json"	
+	]);
+    })
 );
 });
 
